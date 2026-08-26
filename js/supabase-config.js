80c5bb88dd94c2f98a2981a10fc880b72cbf54b3
mido223133331-coder <mido223133331@gmail.com>
2026-08-26 17:32:02 +0000

    Add files via upload

---
+++ js/supabase-config.js
@@ -0,0 +1,6 @@
+window.SUPABASE_CONFIG = {
+    url: 'https://ffqagzsvvgvnvpimpyqz.supabase.co',
+    anonKey: 'sb_publishable_CqvQ3CYSVWWrjXetBd-Rcg_kqwz1dtO',
+    ownerEmail: 'mahmoud229sa@gmail.com',
+    adminPassword: 'MahmoudAdmin2026!'
+};
