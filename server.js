b5abdefdd5ee038b26ac94e3f18154bffc99b3da
mido223133331-coder <mido223133331@gmail.com>
2026-08-27 16:41:26 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const port = process.env.PORT || 3000;
